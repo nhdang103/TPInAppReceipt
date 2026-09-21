615ff14c226884cf75b7624b089c939cb7bec3a1
Nguyen Hai Dang <nhdang103@gmail.com>
2026-09-21 12:17:57 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 
 import PackageDescription
 
 let package = Package(
     name: "TPInAppReceipt",
 	platforms: [.macOS(.v10_13),
-				.iOS(.v12),
+				.iOS(.v15),
 				.tvOS(.v12),
                 .watchOS("6.2"),
                 .visionOS(.v1),
